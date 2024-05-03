6f8e6af0ad5192400e51354cb76e6432652943c0
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-05-03 13:23:18 +0000

    delete in express

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const app = express();
 
 app.use(express.json());
 
-const cars = [{ id: 1, make: "Toyota", model: "Carmy", year: 2022 }];
+const cars = [
+  { id: 1, make: "Toyota", model: "Carmy", year: 2022 },
+  { id: 2, make: "Ford", model: "Mustang", year: 2019 },
+  { id: 3, make: "Chevrolet", model: "Silverado", year: 2020 },
+  { id: 4, make: "Honda", model: "Accord", year: 2021 },
+  { id: 5, make: "BMW", model: "X5", year: 2018 },
+  { id: 6, make: "Tesla", model: "Model S", year: 2023 },
+  { id: 7, make: "Mercedes-Benz", model: "C-Class", year: 2020 },
+  { id: 8, make: "Audi", model: "A4", year: 2019 },
+  { id: 9, make: "Lamborghini", model: "Huracan", year: 2022 },
+  { id: 10, make: "Ferrari", model: "488 GTB", year: 2021 },
+  { id: 11, make: "Porsche", model: "911", year: 2020 },
+  { id: 12, make: "Subaru", model: "Outback", year: 2017 },
+  { id: 13, make: "Jeep", model: "Wrangler", year: 2022 },
+  { id: 14, make: "Volvo", model: "XC90", year: 2021 },
+  { id: 15, make: "Nissan", model: "Altima", year: 2020 },
+];
 
 app.get("/", (req, res) => {
   res.send("hello, Express");
@@ -22,6 +38,19 @@ app.post("/cars", (req, res) => {
 
 app.get("/cars", (req, res) => {
   res.send(cars);
+});
+
+app.delete("/cars/:id", (req, res) => {
+  const carId = req.params.id;
+
+  const index = cars.findIndex((car) => car.id == carId);
+
+  if (index === -1) {
+    res.status(404).json({ error: "car not found" });
+  } else {
+    cars.splice(index, 1);
+    res.status(200).json({ message: "cars deleted successfully" });
+  }
 });
 
 const PORT = 3000;
